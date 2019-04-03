6f6eb61c91ae225da53ce8c90d99f6be4c4fda65
YordanPetkov <49204099+YordanPetkov@users.noreply.github.com>
2019-04-03 15:53:23 +0300

    Add files via upload

---
+++ SqrtNum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+    for(int i=1;i<=20;i++)
+        {int k = sqrt(i);cout<<"Sqrt("<<i<<") = "<<k<<endl;}
+    return 0;
+}
